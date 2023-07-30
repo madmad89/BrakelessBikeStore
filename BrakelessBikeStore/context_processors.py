d0f41305af9686ffe02ff81e4113d6edee0b968c
Madalin P <madalin.pisinaru@gmail.com>
2023-07-30 21:09:21 +0300

    Update context_processors.py

--- BrakelessBikeStore/context_processors.py
+++ BrakelessBikeStore/context_processors.py
@@ -2,4 +2,4 @@ from brakeless_bike.models import Category
 
 
 def navbar_data(request):
-    return {'categories':Category.objects.filter(parent_category=None)}
+    return {'categories': Category.objects.filter(parent_category=None)}
