49bae1e2dd20b802f982f28026fe44b2efdb3aa1
Madalin P <madalin.pisinaru@gmail.com>
2023-08-29 14:26:02 +0300

    update cart

--- brakeless_bike/forms.py
+++ brakeless_bike/forms.py
@@ -4,7 +4,6 @@ from django import forms
 class CheckoutForm(forms.Form):
     shipping_address = forms.CharField(label='Shipping Address', widget=forms.Textarea(attrs={'rows': 3}))
     billing_address = forms.CharField(label='Billing Address', widget=forms.Textarea(attrs={'rows': 3}))
-    city = forms.CharField(label='City', max_length=50)
     card_number = forms.CharField(label='Card Number', max_length=16)
     card_expiry = forms.CharField(label='Card Expiry', max_length=5, help_text='MM/YY')
     card_cvc = forms.CharField(label='Card CVC', max_length=3)
